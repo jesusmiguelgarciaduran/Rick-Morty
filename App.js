2b970b7e41c199a73b52a7fadc78a0eaf6a0f727
Jesús Miguel <Jesusmiguelgarciaduran@gmail.com>
2024-01-08 04:03:25 +0100

    App.js con la estructura de rutas para el intercambio de datos entre las páginas de la aplicación.

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/pages/home';
+import React from 'react';
 import Api from './src/api/api';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DetallePersonaje from './src/pages/detallePersonaje';
+import { useFonts } from 'expo-font';
+const Stack = createStackNavigator();
 export default function App() {
-
+  const [fontsLoaded] = useFonts({
+    'RockSalt-Regular': require("./src/assets/fonts/RockSalt-Regular.ttf")
+  })
+  if (!fontsLoaded) {
+    return null
+  }
   return (
-    <Api/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={Api}
+        >
+        </Stack.Screen>
+        <Stack.Screen
+          name='Detalles del Personaje'
+          component={DetallePersonaje}
+        >
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
 
-});

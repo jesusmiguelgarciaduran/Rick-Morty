d8db25b1dcd827ebefae50f2a1c8f51c94877f02
Jesús Miguel <Jesusmiguelgarciaduran@gmail.com>
2024-01-08 04:02:29 +0100

    Página principal de la aplicación.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,46 +1,63 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View ,Button} from 'react-native';
+import { FlatList, Text, View, Button } from 'react-native';
+import styles from '../styles/mainStyles';
+
 export default function Home(props) {
-        if(props.loading){
-            return (
-                <View style={styles.container}>
-                    <Text>Descargando listado de personajes de Rick & Morty...</Text>
-                </View>
-            );
-        }
-        return(
+   
+    const navigation = useNavigation()
+    if (props.loading) {
+        return (
             <View style={styles.container}>
-                <Text>Listado de personajes de Rick & Morty</Text>
+                <Text>Descargando listado de personajes de Rick & Morty...</Text>
+            </View>
+        );
+    }
+    return (
+        <View style={styles.container}>
+            <Text style={[styles.title]}>Listado de personajes de Rick & Morty</Text>
+            <View style={[styles.list]}>
                 <FlatList
                     data={props.personajes}
                     renderItem={
-                        ({item}) =>
-                        <Text>{item.name}</Text>
+                        ({ item }) =>
+                            <Text 
+                            style={[styles.names]}
+                            onPress={() =>
+                                navigation.navigate('Detalles del Personaje', item)}>{item.name}</Text>
                     }
                 />
-                <Button
-                    title='Anterior'
-                    disabled={props.urlPrev}
-                />
-                <Button
-                    title='Siguiente' 
-                    disabled={props.urlNext} 
-                /> 
-                <Text>{props.url}</Text>
             </View>
-        );
-    
-       
-            
-        
-            
-    
+
+
+            <View style={styles.buttons}>
+                <View style={[styles.width_40]}>
+                    <Button
+                        color={'#3887BE'}
+                        onPress={props.setUrlPrev}
+                        title='Anterior'
+                        disabled={props.urlPrev}
+                    />
+                </View>
+                <View style={[styles.width_40,styles.font]}>
+                    <Button
+                        color={'#3887BE'}
+                        onPress={props.setUrlNext}
+                        title='Siguiente'
+                        disabled={props.urlNext}
+                        titleStyle
+                        
+                    />
+                </View>
+
+            </View>
+        </View>
+    );
+
+
+
+
+
+
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },});
